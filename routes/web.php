15e548ac4ebb366db173c9b1b7e990a50b3cbfcf
NekoMorie <davihanan11@gmail.com>
2024-10-15 16:07:00 +0700

    layout

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('profile/home');
+});
+
+Route::get('/about', function () {
+    return view('profile/about');
 });
